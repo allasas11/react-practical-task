f0dd9d42d7c6de2d53c7655372a086ee608cb623
Alfonsas <alfonsasalubeckas@gmail.com>
2025-03-13 17:22:45 +0200

    customers files rearrangement

--- src/components/ContainerMain.tsx
+++ src/components/ContainerMain.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import styles from "../styles/ContainerMain.module.css"
+import styles from "../styles/customers/ContainerMain.module.css"
 
 
 interface ContainerMainProps {
---
+++ src/components/customers/CustomerForm.tsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/customers/CustomerForm.module.css'
+
+
+const CustomerForm:React.FC = () => {
+    return (
+        <div className={styles.customerFormContainer}>
+            <h1 className={styles.custormerFormTitle}>Customer Form</h1>    
+        </div>
+    )
+}
+export default CustomerForm
