5c7d86ebd03a12bd323776d7ac5680da02d2c9a1
Alfonsas <alfonsasalubeckas@gmail.com>
2025-03-13 17:41:06 +0200

    consumer form added as component

--- src/components/customers/CustomerForm.tsx
+++ src/components/customers/CustomerForm.tsx
@@ -1,11 +1,107 @@
-import styles from '../../styles/customers/CustomerForm.module.css'
+import React from 'react';
+import styles from '../../styles/customers/CustomerForm.module.css';
 
+// Define the type for the props that CustomerForm will receive
+interface CustomerFormProps {
+    formData: {
+        name: string;
+        username: string;
+        email: string;
+        phone: string;
+        address: string;
+        avatar: string;
+    };
+    handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    handleSubmit: (e: React.FormEvent) => void;
+}
 
-const CustomerForm:React.FC = () => {
+const CustomerForm: React.FC<CustomerFormProps> = ({ formData, handleChange, handleSubmit }) => {
     return (
         <div className={styles.customerFormContainer}>
-            <h1 className={styles.custormerFormTitle}>Customer Form</h1>    
+            <form onSubmit={handleSubmit}>
+
+                <div className="form-control">
+                    <label htmlFor="name">Name:</label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+
+                <div className="form-control">
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        name="username"
+                        id="username"
+                        placeholder="Username"
+                        value={formData.username}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+
+                <div className="form-control">
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="Email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+
+                <div className="form-control">
+                    <label htmlFor="phone">Phone:</label>
+                    <input
+                        type="tel"
+                        name="phone"
+                        id="phone"
+                        placeholder="Phone"
+                        value={formData.phone}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+
+                <div className="form-control">
+                    <label htmlFor="address">Address:</label>
+                    <input
+                        type="text"
+                        name="address"
+                        id="address"
+                        placeholder="Address"
+                        value={formData.address}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+
+                <div className="form-control">
+                    <label htmlFor="avatar">Profile image:</label>
+                    <input
+                        type="url"
+                        name="avatar"
+                        id="avatar"
+                        placeholder="Avatar URL"
+                        value={formData.avatar}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+
+                <button type="submit">Create Customer</button>
+            </form>
         </div>
-    )
-}
-export default CustomerForm
+    );
+};
+
+export default CustomerForm;
--- src/pages/customers/EditCustomerPage.tsx
+++ src/pages/customers/EditCustomerPage.tsx
@@ -3,6 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import { Customer } from "../../reducers/customersReducer"; 
 import { useCustomers } from "../../context/CustomersPageContextProvider";
 import styles from '../../styles/customers/EditCustomerPage.module.css'
+import CustomerForm from "../../components/customers/CustomerForm";
 
 
 const EditCustomerPage: React.FC = () => {
@@ -45,69 +46,7 @@ const EditCustomerPage: React.FC = () => {
     return (
         <div>
             <h1 className={styles.pageTitle}>Edit Customer</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="name">Name:</label>
-                    <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        value={customerData.name}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="username">Username:</label>
-                    <input
-                        type="text"
-                        id="username"
-                        name="username"
-                        value={customerData.username}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={customerData.email}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="phone">Phone:</label>
-                    <input
-                        type="text"
-                        id="phone"
-                        name="phone"
-                        value={customerData.phone}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="address">Address:</label>
-                    <input
-                        type="text"
-                        id="address"
-                        name="address"
-                        value={customerData.address}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="avatar">Avatar:</label>
-                    <input
-                        type="text"
-                        id="avatar"
-                        name="avatar"
-                        value={customerData.avatar}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <button type="submit">Save Changes</button>
-            </form>
+            <CustomerForm formData={customerData} handleChange={handleInputChange} handleSubmit={handleSubmit} />
         </div>
     );
 };
